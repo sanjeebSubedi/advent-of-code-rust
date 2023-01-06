327ceb5d0c234f67638cc1bba7912accb7e884b8
Sanjeeb Subedi <sanjeebsubedi4@gmail.com>
2023-01-06 22:55:48 +0545

    function to parse each character of line to vector

--- 2021/src/lib.rs
+++ 2021/src/lib.rs
@@ -16,3 +16,15 @@ where
         .filter_map(|x| x.parse::<T>().ok())
         .collect())
 }
+
+pub fn line_to_char_vec<T, F>(path: &str, func: F) -> Result<Vec<Vec<T>>>
+where
+    T: FromStr,
+    F: Fn(char) -> Option<T>,
+{
+    Ok(BufReader::new(fs::File::open(path)?)
+        .lines()
+        .filter_map(|x| x.ok())
+        .map(|line| line.chars().map(|c| func(c).unwrap()).collect())
+        .collect())
+}
