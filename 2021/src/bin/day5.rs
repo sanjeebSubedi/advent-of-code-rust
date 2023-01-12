9714d93b9cf4c0a122b467d233648f14ce59782d
Sanjeeb Subedi <sanjeebsubedi4@gmail.com>
2023-01-12 18:05:28 +0545

    concise

--- 2021/src/bin/day5.rs
+++ 2021/src/bin/day5.rs
@@ -1,5 +1,4 @@
 use anyhow::Result;
-use itertools::Itertools;
 
 struct Diagram {
     matrix: Vec<Vec<i32>>,
@@ -23,24 +22,16 @@ impl Diagram {
             .fold(0, |acc, x| if x > &1 { acc + 1 } else { acc })
     }
 
-    fn diagonally_add(
-        &mut self,
-        x1: i32,
-        x2: i32,
-        y1: i32,
-        y2: i32,
-        rev_x: bool,
-        rev_y: bool,
-    ) -> Result<()> {
-        let iter_x: Vec<i32> = if rev_x {
-            (x1..=x2).into_iter().rev().collect()
+    fn diagonally_add(&mut self, point: &Vec<i32>) -> Result<()> {
+        let iter_x: Vec<i32> = if point[0] > point[2] {
+            (point[2]..=point[0]).rev().collect()
         } else {
-            (x1..=x2).into_iter().collect()
+            (point[0]..=point[2]).collect()
         };
-        let iter_y: Vec<i32> = if rev_y {
-            (y1..=y2).into_iter().rev().collect()
+        let iter_y: Vec<i32> = if point[1] > point[3] {
+            (point[3]..=point[1]).rev().collect()
         } else {
-            (y1..=y2).into_iter().collect()
+            (point[1]..=point[3]).collect()
         };
         for (x, y) in iter_x.into_iter().zip(iter_y.into_iter()) {
             self.increase_index(x.try_into()?, y.try_into()?);
@@ -57,41 +48,26 @@ impl Diagram {
             }
         }
         if add_diagonally && (point[0] - point[2]).abs() == (point[1] - point[3]).abs() {
-            self.diagonally_add(
-                point[0].min(point[2]),
-                point[0].max(point[2]),
-                point[1].min(point[3]),
-                point[1].max(point[3]),
-                point[0] > point[2],
-                point[1] > point[3],
-            )?
+            self.diagonally_add(point)?
         }
         Ok(())
     }
 }
 
 fn main() -> Result<()> {
-    let mut contents: Vec<Vec<String>> = utils::read_one_per_line::<String>("inputs/day5.txt")?
+    let contents: Vec<Vec<i32>> = utils::read_one_per_line::<String>("inputs/day5.txt")?
         .iter()
-        .map(|line| line.split("->").map(|x| x.trim().to_owned()).collect())
-        .collect();
-    let contents: Vec<Vec<i32>> = contents
-        .iter_mut()
-        .map(|v| {
-            v.iter_mut()
-                .map(|s| {
-                    s.split(",")
-                        .collect_vec()
-                        .iter_mut()
-                        .map(|v| v.parse::<i32>().unwrap())
-                        .collect::<Vec<i32>>()
-                })
-                .flatten()
+        .map(|line| {
+            line.split("->")
+                .map(|x| x.trim())
+                .flat_map(|s| s.split(","))
+                .map(|v| v.parse::<i32>().unwrap())
                 .collect()
         })
         .collect();
-    let max_num = contents.iter().flatten().max();
-    let mut diagram = Diagram::new(max_num.unwrap().to_owned() as usize + 1);
+
+    let max_num = contents.iter().flatten().max().unwrap().to_owned();
+    let mut diagram = Diagram::new(max_num as usize + 1);
     for point in &contents {
         diagram.add_line(point, false)?;
     }
