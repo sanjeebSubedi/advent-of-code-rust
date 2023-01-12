d451b68f8d06d50af70ba346494c7899a95926c1
Sanjeeb Subedi <sanjeebsubedi4@gmail.com>
2023-01-12 15:27:20 +0545

    day 5 complete unomptimized

--- 2021/src/bin/day5.rs
+++ 2021/src/bin/day5.rs
@@ -1,6 +1,5 @@
 use anyhow::Result;
 use itertools::Itertools;
-use std::cmp::Ordering;
 
 struct Diagram {
     matrix: Vec<Vec<i32>>,
@@ -17,30 +16,56 @@ impl Diagram {
         self.matrix[col][row] += 1;
     }
 
-    fn add_line(&mut self, x1: i32, y1: i32, x2: i32, y2: i32) -> Result<()> {
-        match (x1.cmp(&x2), y1.cmp(&y2)) {
-            (Ordering::Less, Ordering::Equal) => {
-                for idx in x1..=x2 {
-                    self.increase_index(idx.try_into()?, y2.try_into()?);
+    fn greater_than_two(&self) -> i32 {
+        self.matrix
+            .iter()
+            .flatten()
+            .fold(0, |acc, x| if x > &1 { acc + 1 } else { acc })
+    }
+
+    fn add_line(&mut self, point: &Vec<i32>, part2: bool) -> Result<()> {
+        if point[0] == point[2] && point[1] == point[3] {
+            return Ok(());
+        }
+        if point[0] == point[2] || point[1] == point[3] {
+            for idx_x in point[0].min(point[2])..=point[0].max(point[2]) {
+                for idx_y in point[1].min(point[3])..=point[1].max(point[3]) {
+                    self.increase_index(idx_x.try_into()?, idx_y.try_into()?);
                 }
             }
-            (Ordering::Greater, Ordering::Equal) => {
-                for idx in x2..=x1 {
-                    self.increase_index(idx.try_into()?, y2.try_into()?);
+        }
+        if part2 && (point[0] - point[2]).abs() == (point[1] - point[3]).abs() {
+            match (point[0] > point[2], point[1] > point[3]) {
+                (true, true) => {
+                    let iter_x = (point[2]..=point[0]).rev().into_iter();
+                    let iter_y = (point[3]..=point[1]).rev().into_iter();
+                    for (x, y) in iter_x.zip(iter_y) {
+                        self.increase_index(x.try_into()?, y.try_into()?);
+                    }
+                }
+                (true, false) => {
+                    let iter_x = (point[2]..=point[0]).rev().into_iter();
+                    let iter_y = (point[1]..=point[3]).into_iter();
+                    for (x, y) in iter_x.zip(iter_y) {
+                        self.increase_index(x.try_into()?, y.try_into()?);
+                    }
+                }
+                (false, true) => {
+                    let iter_x = (point[0]..=point[2]).into_iter();
+                    let iter_y = (point[3]..=point[1]).rev().into_iter();
+                    for (x, y) in iter_x.zip(iter_y) {
+                        self.increase_index(x.try_into()?, y.try_into()?);
+                    }
+                }
+                (false, false) => {
+                    let iter_x = (point[0]..=point[2]).into_iter();
+                    let iter_y = (point[1]..=point[3]).into_iter();
+                    for (x, y) in iter_x.zip(iter_y) {
+                        self.increase_index(x.try_into()?, y.try_into()?);
+                    }
                 }
             }
-            (Ordering::Equal, Ordering::Less) => {
-                for idx in y1..=y2 {
-                    self.increase_index(x1.try_into()?, idx.try_into()?);
-                }
-            }
-            (Ordering::Equal, Ordering::Greater) => {
-                for idx in y2..=y1 {
-                    self.increase_index(x1.try_into()?, idx.try_into()?);
-                }
-            }
-            (_, _) => {}
-        };
+        }
         Ok(())
     }
 }
@@ -50,7 +75,7 @@ fn main() -> Result<()> {
         .iter()
         .map(|line| line.split("->").map(|x| x.trim().to_owned()).collect())
         .collect();
-    let contents: Vec<Vec<Vec<i32>>> = contents
+    let contents: Vec<Vec<i32>> = contents
         .iter_mut()
         .map(|v| {
             v.iter_mut()
@@ -59,21 +84,22 @@ fn main() -> Result<()> {
                         .collect_vec()
                         .iter_mut()
                         .map(|v| v.parse::<i32>().unwrap())
-                        .collect()
+                        .collect::<Vec<i32>>()
                 })
+                .flatten()
                 .collect()
         })
         .collect();
-    let max_num = contents.iter().flatten().flatten().max();
-    let mut d = Diagram::new(max_num.unwrap().to_owned() as usize + 1);
-    for content in contents {
-        d.add_line(content[0][0], content[0][1], content[1][0], content[1][1])?;
+    let max_num = contents.iter().flatten().max();
+    let mut d1 = Diagram::new(max_num.unwrap().to_owned() as usize + 1);
+    for point in &contents {
+        d1.add_line(point, false)?;
     }
-    let greater_than_2 = d
-        .matrix
-        .iter()
-        .flatten()
-        .fold(0, |acc, x| if x > &1 { acc + 1 } else { acc });
-    println!("Part 1: {}", greater_than_2);
+    let mut d2 = Diagram::new(max_num.unwrap().to_owned() as usize + 1);
+    for point in &contents {
+        d2.add_line(point, true)?;
+    }
+    println!("Part 1: {}", d1.greater_than_two());
+    println!("Part 2: {}", d2.greater_than_two());
     Ok(())
 }
